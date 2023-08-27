e344e91f7ae4bc1a6333e1ddb3af1976a2a10595
halu <hlu07472@gmail.com>
2023-08-27 22:44:57 +0800

    Add image white list and wrapped image process

--- api/images_api/images_upload.go
+++ api/images_api/images_upload.go
@@ -3,15 +3,61 @@ package images_api
 import (
 	"blog_server/global"
 	"blog_server/models/res"
+	"blog_server/utils"
 	"fmt"
+	"io/fs"
+	"mime/multipart"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	ImageWhiteList = []string{
+		"jpg",
+		"png",
+		"jpeg",
+		"ico",
+		"tiff",
+		"gif",
+		"svg",
+		"webp",
+	}
 )
 
 type FileUploadResponse struct {
 	FileName   string `json:"file_name"`
 	IsUploaded bool   `json:"is_uploaded"`
 	Msg        string `json:"msg"`
+}
+
+func processImage(image *multipart.FileHeader) FileUploadResponse {
+	nameList := strings.Split(image.Filename, ".")
+	suffix := strings.ToLower(nameList[len(nameList)-1])
+
+	if !utils.InList(suffix, ImageWhiteList) {
+		return FileUploadResponse{
+			FileName:   image.Filename,
+			IsUploaded: false,
+			Msg:        "Illegal file type",
+		}
+	}
+
+	size := float64(image.Size) / float64(1024*1024)
+	if size > float64(global.Config.Upload.Size) {
+		return FileUploadResponse{
+			FileName:   image.Filename,
+			IsUploaded: false,
+			Msg:        fmt.Sprintf("File size exceeds the maximum size %d", global.Config.Upload.Size),
+		}
+	}
+
+	return FileUploadResponse{
+		FileName:   image.Filename,
+		IsUploaded: true,
+		Msg:        "File uploaded successfully!",
+	}
 }
 
 func (ImagesApi) ImagesUploadView(c *gin.Context) {
@@ -30,24 +76,18 @@ func (ImagesApi) ImagesUploadView(c *gin.Context) {
 		return
 	}
 
+	basePath := global.Config.Upload.Path
+	_, err = os.ReadDir(basePath)
+	if err != nil {
+		err = os.MkdirAll(basePath, fs.ModePerm)
+		if err != nil {
+			global.Logger.Error(err)
+		}
+	}
+
 	var responseList []FileUploadResponse
 	for _, image := range imageList { // image is a file header type
-		//filePath := path.Join("uploads", image.Filename)
-		size := float64(image.Size) / float64(1024*1024)
-
-		if size > float64(global.Config.Upload.Size) {
-			responseList = append(responseList, FileUploadResponse{
-				FileName:   image.Filename,
-				IsUploaded: false,
-				Msg:        fmt.Sprintf("File size exceeds the maximum size %d", global.Config.Upload.Size),
-			})
-		} else {
-			responseList = append(responseList, FileUploadResponse{
-				FileName:   image.Filename,
-				IsUploaded: true,
-				Msg:        "File uploaded successfully!",
-			})
-		}
+		responseList = append(responseList, processImage(image))
 	}
 
 	res.OkWithData(responseList, c)
