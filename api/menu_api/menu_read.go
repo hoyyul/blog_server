aadd338bed474a77051b20c3209ebc408a89553e
halu <hlu07472@gmail.com>
2023-09-01 17:33:31 +0800

    Add API for getting single menu info

---
+++ api/menu_api/menu_read.go
@@ -0,0 +1,38 @@
+package menu_api
+
+import (
+	"blog_server/global"
+	"blog_server/models"
+	"blog_server/models/res"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (MenuApi) MenuGetView(c *gin.Context) {
+	// search menu table
+	id := c.Param("id")
+	var menuModel models.MenuModel
+	err := global.DB.Take(&menuModel, id).Error
+	if err != nil {
+		res.FailWithMessage("menu doesn't exist", c)
+		return
+	}
+	// search menu-banner table
+	var menuBanners []models.MenuBannerModel
+	global.DB.Preload("BannerModel").Order("sort desc").Find(&menuBanners, "menu_id = ?", id)
+	var banners = make([]Banner, 0)
+	for _, record := range menuBanners {
+		if menuModel.ID != record.MenuID {
+			continue
+		}
+		banners = append(banners, Banner{
+			ID:   record.BannerID,
+			Path: record.Banner.Path,
+		})
+	}
+	menuResponse := MenuResponse{
+		MenuModel: menuModel,
+		Banners:   banners,
+	}
+	res.OkWithData(menuResponse, c)
+}
--- routers/menu_router.go
+++ routers/menu_router.go
@@ -7,6 +7,7 @@ import (
 func (rg RouterGroup) MenuRouter() {
 	app := api.ApiGroupApp.MenuApi
 	rg.POST("menu", app.MenuCreateView)
+	rg.GET("menu/:id", app.MenuGetView)
 	rg.GET("menu", app.MenuReadListView)
 	rg.GET("menu_name", app.MenuReadNameList)
 	rg.PUT("menu/:id", app.MenuUpdateView)
