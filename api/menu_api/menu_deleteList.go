257c9b7bee4d6554201383384b67eb344c0ddcad
halu <hlu07472@gmail.com>
2023-09-01 17:29:35 +0800

    Add API for removing menu list

---
+++ api/menu_api/menu_deleteList.go
@@ -0,0 +1,49 @@
+package menu_api
+
+import (
+	"blog_server/global"
+	"blog_server/models"
+	"blog_server/models/res"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func (MenuApi) MenuRemoveView(c *gin.Context) {
+	var req models.RemoveRequest
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		res.FailWithCode(res.ParameterError, c)
+		return
+	}
+
+	var menuList []models.MenuModel
+	count := global.DB.Find(&menuList, req.IdList).RowsAffected
+	if count == 0 {
+		res.FailWithMessage("Menu doesn't exist", c)
+		return
+	}
+	fmt.Println("idlist", req.IdList)
+	// transaction to delete data in menu-banner table and menu table
+	err = global.DB.Transaction(func(tx *gorm.DB) error {
+		err = global.DB.Model(&menuList).Association("Banners").Clear()
+		if err != nil {
+			global.Logger.Error(err)
+			return err
+		}
+		err = global.DB.Delete(&menuList).Error
+		if err != nil {
+			global.Logger.Error(err)
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		global.Logger.Error(err)
+		res.FailWithMessage("Failed to delete menu", c)
+		return
+	}
+	res.OkWithMessage(fmt.Sprintf("Remove %d menus", count), c)
+
+}
--- routers/menu_router.go
+++ routers/menu_router.go
@@ -10,4 +10,5 @@ func (rg RouterGroup) MenuRouter() {
 	rg.GET("menu", app.MenuReadListView)
 	rg.GET("menu_name", app.MenuReadNameList)
 	rg.PUT("menu/:id", app.MenuUpdateView)
+	rg.DELETE("menu", app.MenuRemoveView)
 }
